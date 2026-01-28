a586783c47400759c7c8c246f4412e8beea58c24
aditya-3761 <aditya6112005@gmail.com>
2026-01-28 15:22:53 +0530

    fetch api revision

--- day_01/promises/index.js
+++ day_01/promises/index.js
@@ -1,15 +1,30 @@
-const myPromise = new Promise((resolve, reject) => {
-    let marks = 80;
-    if (marks >= 50) {
-        resolve("You have passed the exam");
-    } else {
-        reject("You have failed the exam");
-    }
-})
+const getData = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const res = await response.json();
+    console.log(res);
+}
+getData();
+
+
+
+// const myPromise = new Promise((resolve, reject) => {
+//     let marks = 80;
+//     if (marks >= 50) {
+//         resolve("You have passed the exam");
+//     } else {
+//         reject("You have failed the exam");
+//     }
+// })
 // console.log(myPromise);
 
-myPromise.then((message) => {
-    console.log(message);
-}).catch((error) => {
-    console.log(error);
-});
+// myPromise.then((message) => {
+//     console.log(message);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+// const f1 = async () => {
+//     const msg = await myPromise;
+//     console.log(msg);
+// }
+// f1();
